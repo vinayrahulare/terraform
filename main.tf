284733d001acef075c6daf2120e20e994ece169b
Vinay Are <VAre@teladoc.com>
2022-04-06 16:25:19 -0400

    adding main.tf and variables.tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+#AWS Instance
+resource "aws_instance" "example" {
+    ami = data.aws_ami.windows.id
+    instance_type = "t2.micro"
+    availability_zone = var.availability_zone
+  
+  lifecycle {
+    ignore_changes = [ami]
+  }
+}
+
+#AMI Filter for Windows Server 2019 Base
+data "aws_ami" "windows" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2019-English-Full-Base-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["801119661308"] # Canonical
+}
+
+#EBS Volume and Attachment
+
+resource "aws_ebs_volume" "example" {
+  availability_zone = var.availability_zone
+  size              = 30
+}
+
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example.id
+  instance_id = aws_instance.example.id
+}
